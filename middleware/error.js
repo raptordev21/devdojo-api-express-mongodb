13a067d52f60522266c9945619c415e052f5857f
Himanshu Choudhary <raptordev21@gmail.com>
2022-02-13 11:38:21 +0530

    added resource ownership

--- middleware/error.js
+++ middleware/error.js
@@ -6,7 +6,9 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     // Log to console for dev
-    console.log(err.stack.red);
+    if (process.env.NODE_ENV === 'development') {
+        console.log(err.stack.red);
+    }
 
     // Mongoose invalid ObjectId
     if (err.name === 'CastError') {
