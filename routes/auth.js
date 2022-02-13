7f7ae36f653d0e8d7fc83f968245a06afef7187d
Himanshu Choudhary <raptordev21@gmail.com>
2022-02-13 20:52:26 +0530

    added user crud for admin

--- routes/auth.js
+++ routes/auth.js
@@ -16,10 +16,10 @@ const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-// router.get('/logout', logout);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
-// router.put('/updatedetails', protect, updateDetails);
-// router.put('/updatepassword', protect, updatePassword);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', require('./routes/bootcamps'));
 app.use('/api/v1/courses', require('./routes/courses'));
 app.use('/api/v1/auth', require('./routes/auth'));
-// app.use('/api/v1/users', require('./routes/users'));
+app.use('/api/v1/users', require('./routes/users'));
 // app.use('/api/v1/reviews', require('./routes/reviews'));
 
 // Custom error handling middleware, must placed after routes
