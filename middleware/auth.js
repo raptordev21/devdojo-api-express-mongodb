5bc3b6f9ae9c451aa7a9918acb2c19bdd4755ef3
Himanshu Choudhary <raptordev21@gmail.com>
2022-02-15 12:17:14 +0530

    added documentation

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,14 +7,16 @@ const User = require('../models/User');
 exports.protect = asyncHandler(async (req, res, next) => {
     let token;
 
+    // Set token from Bearer token in header
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        // Set token from Bearer token in header
         token = req.headers.authorization.split(' ')[1];
     }
 
-    // Set token from cookie
-    else if (req.cookies.token) {
-        token = req.cookies.token;
+    if (!token) {
+        // Set token from cookie
+        if (req.cookies.token) {
+            token = req.cookies.token;
+        }
     }
 
     // Make sure token exists
